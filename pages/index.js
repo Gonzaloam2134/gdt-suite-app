292444ea8b8a7c4e44c8726060ae0b066529d149
Gonzaloam2134 <gonzaloam1@outlook.com>
2026-08-04 17:57:39 -0300

    Import useRouter from next/router

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { supabase } from '../lib/supabaseClient'
 import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function Home() {
   const [user, setUser] = useState(null)
